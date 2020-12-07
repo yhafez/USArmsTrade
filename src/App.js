d901379d375ca6a86414e37c70528e9e1280f635
Taryn Martin <taryn.martin919@gmail.com>
2020-12-07 14:33:47 -0700

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import HeatMap from "./HeatMap";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <HeatMap />
     </div>
   );
